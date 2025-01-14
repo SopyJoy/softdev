d48af8140c25c620683fc1a61d3b2838672c49ca
Sofia Joy Yunun <yununsofiajoy1521@gmail.com>
2025-01-14 11:13:08 +0800

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
-  future: {
-    compatibilityVersion: 4,
-  },
-
-  experimental: {
-      scanPageMeta: 'after-resolve',
-      sharedPrerenderData: false,
-      compileTemplate: true,
-      resetAsyncDataToUndefined: true,
-      templateUtils: true,
-      relativeWatchPaths: true,
-      normalizeComponentNames: false,
-      spaLoadingTemplateLocation: 'within',
-      defaults: {
-        useAsyncData: {
-          deep: true
-        }
-      }
-    },
-
-  features: {
-    inlineStyles: true
-  },
-
-  unhead: {
-    renderSSRHeadOptions: {
-      omitLineBreaks: false
-    }
-  },
-
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts',
-    '@nuxt/image',
-    '@nuxt/icon'
-  ],
-
-  googleFonts:{
-    families:{
-      Montserrat:true,
-    }
-  }
+  compatibilityDate: "2025-01-14"
 })
